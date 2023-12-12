5cb8f6e0cc89193e5b0cffd85c88e50198ba80ce
Adib Haider <adib.haider@acast.com>
2023-12-12 10:40:35 +0100

    mv nvm-cmp

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -2,18 +2,6 @@ return {
 	{
 		'williamboman/mason.nvim',
 		config = true,
-		dependencies = {
-			-- Autocompletion
-			'hrsh7th/nvim-cmp',
-			'hrsh7th/cmp-nvim-lsp',
-
-			-- Snippets
-			'L3MON4D3/LuaSnip',
-			'hrsh7th/cmp-nvim-lsp',
-
-			-- Adds a number of user-friendly snippets
-			'rafamadriz/friendly-snippets',
-		},
 	},
 	"williamboman/mason-lspconfig.nvim",
 	"neovim/nvim-lspconfig",
