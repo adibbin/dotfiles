50a1939bbe0a1cf6b8f680940e2be127e08e0cfc
Adib Haider <adib.haider@acast.com>
2024-01-07 22:47:44 +0100

    Update copilot and nvim-cmp configs

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,8 +1,31 @@
 return {
 	"zbirenbaum/copilot.lua",
 	cmd = "Copilot",
+	build = ":Copilot auth",
 	event = "InsertEnter",
 	config = function()
-		require("copilot").setup({})
+		require("copilot").setup({
+			panel = {
+				enabled = true,
+				auto_refresh = true,
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				accept = false, --disable built-in keymapping
+			},
+		})
+
+		-- disable Copilot for cmp menu
+		local has_cmp, cmp = pcall(require, "cmp")
+		if has_cmp then
+			cmp.event:on("menu_opened", function()
+				vim.b.copilot_suggestion_hidden = true
+			end)
+
+			cmp.event:on("menu_closed", function()
+				vim.b.copilot_suggestion_hidden = false
+			end)
+		end
 	end,
 }
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -8,48 +8,32 @@ return {
 		local luasnip = require 'luasnip'
 
 		cmp.setup({
-			completion = {
-				completeopt = 'menu,menuone,noinsert',
-			},
-			mapping = cmp.mapping.preset.insert({
-				['<C-n>'] = cmp.mapping.select_next_item(),
-				['<C-p>'] = cmp.mapping.select_prev_item(),
-				['<C-d>'] = cmp.mapping.scroll_docs(-4),
-				['<C-f>'] = cmp.mapping.scroll_docs(4),
-				['<C-Space>'] = cmp.mapping.complete {},
-				['<CR>'] = cmp.mapping.confirm {
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = true,
-				},
-				['<Tab>'] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.expand_or_locally_jumpable() then
-							luasnip.expand_or_jump()
-						else
-							fallback()
-						end
-					end,
-					{ 'i', 's' }),
-				['<S-Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { 'i', 's' }),
-			}),
 			snippet = {
 				expand = function(args)
 					require('luasnip').lsp_expand(args.body)
 				end,
 			},
-			sources = {
+			mapping = cmp.mapping.preset.insert({
+				['<C-Space>'] = cmp.mapping.complete(),
+				['<CR>'] = cmp.mapping.confirm({ select = true }),
+				['<Tab>'] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
+					elseif require("copilot.suggestion").is_visible() then
+						require("copilot.suggestion").accept()
+					elseif luasnip.expand_or_locally_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { 'i', 's' }),
+			}),
+			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
 				{ name = 'luasnip' },
-			},
+			}, {
+				{ name = 'buffer' },
+			})
 		})
 	end
 }
