4166bf342bde069122c0c24f5aa06f4c4d1b5352
Adib Haider <adib.haider@acast.com>
2024-01-08 21:26:15 +0100

    Update LSP configuration

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -4,8 +4,8 @@ return {
 		'hrsh7th/cmp-nvim-lsp',
 	},
 	config = function()
-		local cmp = require 'cmp'
-		local luasnip = require 'luasnip'
+		local cmp = require('cmp')
+		local luasnip = require('luasnip')
 
 		cmp.setup({
 			snippet = {
---
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,44 @@
+return {
+	'neovim/nvim-lspconfig',
+	dependencies = {
+		{ 'williamboman/mason.nvim', config = true },
+		'williamboman/mason-lspconfig.nvim',
+	},
+	config = function()
+		require('mason').setup()
+		require('mason-lspconfig').setup()
+
+		local servers = {
+			elixirls = {},
+			lua_ls = {
+				Lua = {
+					diagnostics = {
+						globals = { 'vim' },
+					},
+					workspace = { checkThirdParty = false },
+					telemetry = { enable = false },
+				},
+			},
+			tsserver = {},
+		}
+
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+		local mason_lspconfig = require 'mason-lspconfig'
+		mason_lspconfig.setup {
+			ensure_installed = vim.tbl_keys(servers),
+		}
+
+		mason_lspconfig.setup_handlers {
+			function(server_name)
+				require('lspconfig')[server_name].setup {
+					capabilities = capabilities,
+					on_attach = require('config.keymap').on_attach,
+					settings = servers[server_name],
+					filetypes = (servers[server_name] or {}).filetypes,
+				}
+			end,
+		}
+	end,
+}
