edd5944eefee384169b20fb76bd85e104ef27a28
Adib Haider <adibbinhaider@gmail.com>
2024-02-27 11:25:39 +0100

    add :TSUpdate on build

--- stow/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ stow/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,37 +1,35 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-		end,
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-context",
-		},
-		opts = {
-			ensure_installed = {
-				'c',
-				'elixir',
-				'go',
-				'heex',
-				'javascript',
-				'lua',
-				'markdown',
-				'markdown_inline',
-				'python',
-				'query',
-				'tsx',
-				'typescript',
-				'vim',
-				'vimdoc',
-			},
-			highlight = {
-				enable = true,
-				use_languagetree = true,
-			},
-			indent = {
-				enable = true
-			},
-		}
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
+	end,
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-context",
 	},
-
+	opts = {
+		ensure_installed = {
+			'c',
+			'elixir',
+			'go',
+			'heex',
+			'javascript',
+			'lua',
+			'markdown',
+			'markdown_inline',
+			'python',
+			'query',
+			'tsx',
+			'typescript',
+			'vim',
+			'vimdoc',
+		},
+		highlight = {
+			enable = true,
+			use_languagetree = true,
+		},
+		indent = {
+			enable = true
+		},
+	}
 }
