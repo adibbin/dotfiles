759ecf43e420a2ceeee578511fcf5646074aca65
Adib Haider <adibbinhaider@gmail.com>
2024-02-27 16:41:42 +0100

    fmt on save

--- stow/.config/nvim/lua/plugins/conform.lua
+++ stow/.config/nvim/lua/plugins/conform.lua
@@ -5,6 +5,10 @@ return {
 		"BufWritePre",
 	},
 	opts = {
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
 		formatters_by_ft = {
 			lua = { "stylelua" },
 			python = { "ruff_format" },
