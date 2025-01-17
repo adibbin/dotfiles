50fdbcdddb3549b21bf1fce245ac4a6cd4bf42e6
Adib Haider <adibbinhaider@gmail.com>
2025-01-17 20:26:55 +0100

    nvim: add qf keybindings

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -2,6 +2,11 @@ local nmap = function(keys, func, desc)
 	vim.keymap.set('n', keys, func, { desc = desc })
 end
 
+-- Quickfix
+nmap('<M-j>', '<cmd>cnext<CR>', 'Move to the next quickfix item')
+nmap('<M-k>', '<cmd>cprev<CR>', 'Move to the previous quickfix item')
+
+-- Window navigation
 nmap('<C-h>', '<C-w><C-h>', 'Move focus to the left window')
 nmap('<C-l>', '<C-w><C-l>', 'Move focus to the right window')
 nmap('<C-j>', '<C-w><C-j>', 'Move focus to the lower window')
